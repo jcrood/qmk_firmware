e72ef3f724e042ab735f1143a64aa2d33ce3d3c8
John Rood <j.rood@picturae.com>
2023-11-01 15:43:17 +0100

    DROPALT: initial import

---
+++ keyboards/drop/alt/keymaps/jcrood/config.h
@@ -0,0 +1,11 @@
+// drop alt v2
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD 
+
+
+// RGB stuffs
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
---
+++ keyboards/drop/alt/keymaps/jcrood/rules.mk
@@ -0,0 +1,16 @@
+# drop alt v2
+
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+NKRO_ENABLE = no           # Enable N-Key Rollover
+AUTO_SHIFT_ENABLE = no      # Auto Shift
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+TAP_DANCE_ENABLE = no
+DEBUG_EEPROM = no
+CAPS_WORD_ENABLE = yes
+
