699602c20a3cc041844d44ba329ce068f9c0f4ee
John Rood <j.rood@picturae.com>
2023-11-01 08:18:10 +0100

    KEYCHRON: add EE_CLR, drop NK_TOGG from _KEYB layer. fixup rules.mk

--- keyboards/keychron/q2/ansi/keymaps/jcrood/rules.mk
+++ keyboards/keychron/q2/ansi/keymaps/jcrood/rules.mk
@@ -2,15 +2,19 @@
 
 DIP_SWITCH_ENABLE = no 		# kill mac/win switch
 
-BOOT_MAGIC = yes
-CONSOLE_ENABLE = no
-NKRO_ENABLE = yes
-MOUSEKEY_ENABLE = no
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes		# fancy rgb stuffs
+AUTO_SHIFT_ENABLE = no      # Auto Shift
 TAP_DANCE_ENABLE = no
-LTO_ENABLE = no
-UCIS_ENABLE = no
-COMMAND_ENABLE = yes
-DEBUG_EEPROM = no
-RGB_MATRIX_ENABLE = yes
-CAPS_WORD_ENABLE = yes
+CAPS_WORD_ENABLE = yes		# also add #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD to config.h
 
+# debug
+
+DEBUG_EEPROM = no
+CONSOLE_ENABLE = no         # Console for debug
