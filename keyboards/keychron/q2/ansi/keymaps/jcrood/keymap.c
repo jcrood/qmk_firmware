406adf30778b1bc4041b33d6ab3d6b20f16bac13
John Rood <j.rood@picturae.com>
2023-11-01 12:19:59 +0100

    Make _ADJUST layer tristate

--- keyboards/keychron/q2/ansi/keymaps/jcrood/keymap.c
+++ keyboards/keychron/q2/ansi/keymaps/jcrood/keymap.c
@@ -13,7 +13,7 @@ enum layers{
     _BASE,
     _FN,
     _MAC,
-    _KEYB,
+    _ADJUST,
     _FN4
 };
 
@@ -26,7 +26,7 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ansi_67(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          MO(_KEYB),
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          KC_DEL,
         FN_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_PGUP,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______,  _______, _______, _______, _______),
 
 
-    [_KEYB] = LAYOUT_ansi_67(
+    [_ADJUST] = LAYOUT_ansi_67(
         _______, KC_BRID, KC_BRIU, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,          _______,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______, _______,          _______,
         _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,           _______,          _______,
@@ -55,13 +55,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______,  _______, _______, _______, _______),
 
     [_FN4] = LAYOUT_ansi_67(
-        CMDGRV,  _______, CAPSCR,  CAPSEL,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        CMDGRV,  _______, _______, CAPSCR,  CAPSEL,   _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, LOCKSCR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,          _______,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,           _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, UPDIR,   _______,           _______, _______,
         _______, _______, _______,                            _______,                            _______, _______,  _______, _______, _______, _______)
 
 };
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _FN, _MAC, _ADJUST);
+}
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
@@ -89,7 +95,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                         case _FN4:
                             rgb_matrix_set_color(index, RGB_GREEN);
                             break;
-                        case _KEYB:
+                        case _ADJUST:
                             rgb_matrix_set_color(index, RGB_RED);
                             break;
                         case _MAC:
