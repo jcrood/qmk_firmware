4316f0e5697093815a3734eb86c747395c8b1e83
John Rood <j.rood@picturae.com>
2023-11-04 00:19:57 +0100

    ANNEPRO: Add UPDIR macro, bring rules.mk in sync

--- keyboards/annepro2/keymaps/jcrood/rules.mk
+++ keyboards/annepro2/keymaps/jcrood/rules.mk
@@ -1,10 +1,19 @@
-CONSOLE_ENABLE = no
-NKRO_ENABLE = yes
-MOUSEKEY_ENABLE = no
-TAP_DANCE_ENABLE = no
-LTO_ENABLE = no
-UCIS_ENABLE = no
-COMMAND_ENABLE = yes
-DEBUG_EEPROM = no
-CAPS_WORD_ENABLE = yes
+# anne pro features
 
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+COMMAND_ENABLE = yes         # Commands for debug and configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = no        # Mouse keys
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes		# fancy rgb stuffs
+AUTO_SHIFT_ENABLE = no      # Auto Shift
+TAP_DANCE_ENABLE = no
+
+CAPS_WORD_ENABLE = yes		# also add #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD to config.h
+
+# debug
+
+DEBUG_EEPROM = no
+CONSOLE_ENABLE = no         # Console for debug
