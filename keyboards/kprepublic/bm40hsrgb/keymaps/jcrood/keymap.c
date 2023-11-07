31754b276061f7ada964d74658c18da9d1ba873e
John Rood <jcr@hq.sfarc.net>
2023-11-07 23:16:51 +0100

    BM40: move planck stuff to own .h

--- keyboards/kprepublic/bm40hsrgb/keymaps/jcrood/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/jcrood/keymap.c
@@ -2,7 +2,7 @@
 
 #include QMK_KEYBOARD_H
 #include "jcrood.h"
-
+#include "planck.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_planck_wrapper(M_QWERTY),
