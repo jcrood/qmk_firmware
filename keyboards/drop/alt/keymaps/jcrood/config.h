c34357e8dd949b6122caee7714dd2d5cb68d4d1a
John Rood <jcr@hq.sfarc.net>
2023-11-09 23:39:12 +0100

    DROPALT: add userspace stuff

--- keyboards/drop/alt/keymaps/jcrood/config.h
+++ keyboards/drop/alt/keymaps/jcrood/config.h
@@ -1,9 +1,10 @@
 // drop alt v2
 #pragma once
+#include "rgb_stuff.h"
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
-#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD 
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 
 
 // RGB stuffs
