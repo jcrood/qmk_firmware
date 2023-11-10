67b0d7f92e3aeb914a9fb6c36f06a46dca8aa611
John Rood <jcr@hq.sfarc.net>
2023-11-10 23:30:21 +0100

    USER: Add UPDIR to userspace

--- users/jcrood/jcrood.c
+++ users/jcrood/jcrood.c
@@ -1,6 +1,17 @@
 #include "jcrood.h"
 
-void my_custom_function(void) {
-    
+__attribute__ ((weak))
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    switch (keycode) {
+        case M_UPDIR:  // Types ../ to go up a directory on the shell.
+            if (record->event.pressed) {
+                SEND_STRING("../");
+            }
+            return false;
+    }
+  return process_record_keymap(keycode, record);
+}
--- users/jcrood/jcrood.h
+++ users/jcrood/jcrood.h
@@ -3,14 +3,15 @@
 
 #include "quantum.h"
 
+enum custom_keycodes {
+    M_UPDIR = SAFE_RANGE,
+    NEW_SAFE_RANGE  //use "NEW_SAFE_RANGE" for keymap specific codes
+};
+
 // mac stuff
 #define CMDGRV LGUI(KC_GRV)             // cmd ` - focus to next window
 #define LOCKSCR LCTL(LGUI(KC_Q))        // ctrl cmd Q  - lock screen
 #define CAPSCR SGUI(KC_3)               // shift cmd 3 - capture screen
 #define CAPSEL SGUI(KC_4)               // shift cmd 4 - capture selection
-
-
-
-void my_custom_function(void);
 
 #endif
